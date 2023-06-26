1415a06f8a65762a8c318599c24cf4284b9fddbb
UHquokka <bananamayong@gmail.com>
2023-06-26 16:29:17 +0900

    useEffect 사용법

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppProfile from './AppProfile';
 import AppCounter from './AppCounter';
-import './App.css';
+import './index.css';
+import AppProducts from './AppProducts';
+
 
 //html 에 있던 root 를 가져와서, 거기에 루트를 생성해준다. 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //strict 모드는 개발하는 동안에만 켜져있고, 배포할 때 꺼짐. 
   <React.StrictMode>
-    <AppCounter />
+    <AppProducts />
   </React.StrictMode>
 );
