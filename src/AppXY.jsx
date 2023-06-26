a248b3738729168fda7238593195c04be6c7fe5d
UHquokka <bananamayong@gmail.com>
2023-06-26 22:07:07 +0900

    연관된 데이터 객체로 묶기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,16 +2,14 @@ import React, { useState } from 'react';
 import './AppXY.css'
 
 export default function AppXY() {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
 
     const handleMouseMove = (e) => {
-        setX(e.clientX + 15);
-        setY(e.clientY + 15);
+        setPosition({ x: e.clientX, y: e.clientY })
     }
     return (
         <div className='container' onMouseMove={handleMouseMove}>
-            <div className='pointer' style={{ left: x, top: y }} />
+            <div className='pointer' style={{ transform: `translate(${position.x}px, ${position.y}px)` }} />
         </div>
     );
 }
