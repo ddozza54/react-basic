a8fc7b0520124eb344cff870d3873a4b5bccd075
UHquokka <bananamayong@gmail.com>
2023-06-27 13:10:36 +0900

    immer 를 사용해서 state update 하기

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import AppXY from './AppXY';
 import AppMentor from './AppMentor';
 import AppMentors from './AppMentors';
+import AppMentorsImmer from './AppMentorsImmer';
 
 
 //html 에 있던 root 를 가져와서, 거기에 루트를 생성해준다. 
@@ -11,6 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //strict 모드는 개발하는 동안에만 켜져있고, 배포할 때 꺼짐. 
   <React.StrictMode>
-    <AppMentors />
+    <AppMentorsImmer />
   </React.StrictMode>
 );
