2642e432db6214661cd29a88596a13e9544aa16d
UHquokka <bananamayong@gmail.com>
2023-06-27 16:24:33 +0900

    form의 value를 하나의 객체로 만들기

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { forwardRef, useState } from 'react';
 
 export default function AppForm() {
-    const handleSubmit = (e) => e.preventDefault();
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    const [form, setForm] = useState({ name: '', email: '' })
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(form)
+    };
+    const handleChange = (e) => {
+        // 속성 name 을 뜻함. 이름, 이메일의 name 아님. 
+        const { name, value } = e.target;
+        setForm(({ ...form, [name]: value }))
+    }
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor='name'>이름:</label>
-            <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} />
+            <input type='text' id='name' name='name' value={form.name} onChange={handleChange} />
             <label htmlFor='email'>이메일:</label>
-            <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type='email' id='email' name='email' value={form.email} onChange={handleChange} />
             <button>Submit</button>
         </form>
     );
