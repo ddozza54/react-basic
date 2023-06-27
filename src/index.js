79250dfeef14ca08a0589d045dd56f7c4efc73f0
UHquokka <bananamayong@gmail.com>
2023-06-27 09:31:02 +0900

    멘도 name, title 바꾸기 - self

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppXY from './AppXY';
+import AppMentor from './AppMentor';
 
 
 //html 에 있던 root 를 가져와서, 거기에 루트를 생성해준다. 
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //strict 모드는 개발하는 동안에만 켜져있고, 배포할 때 꺼짐. 
   <React.StrictMode>
-    <AppXY />
+    <AppMentor />
   </React.StrictMode>
 );
