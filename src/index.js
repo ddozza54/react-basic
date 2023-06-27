666ba19adb4b05e1cfc26a71aed8d0e60c5dc57a
UHquokka <bananamayong@gmail.com>
2023-06-27 16:10:38 +0900

    input 과 state - 1단계

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import AppXY from './AppXY';
 import AppMentor from './AppMentor';
 import AppMentors from './AppMentors';
 import AppMentorsImmer from './AppMentorsImmer';
+import AppForm from './AppForm';
 
 
 //html 에 있던 root 를 가져와서, 거기에 루트를 생성해준다. 
@@ -12,6 +13,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //strict 모드는 개발하는 동안에만 켜져있고, 배포할 때 꺼짐. 
   <React.StrictMode>
-    <AppMentorsImmer />
+    <AppForm />
   </React.StrictMode>
 );
